9fd897d30e3c78d5e8852c5abebea4a99b9ef289
Abhinav Krishna <55917035+AbhinavKrishna26@users.noreply.github.com>
2020-09-16 22:29:14 +0530

    Adds project page (#5)
    Co-authored-by: arc9693 <archana.choudhary.9693@gmail.com>

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () { 
-    jQuery('#status').fadeOut(7000); 
-    $('#preloader').delay(1000).fadeOut(5000); 
+    jQuery('#status').fadeOut(900); 
+    $('#preloader').delay(2).fadeOut(1000); 
     $('body').delay(1000).css({'overflow':'visible'});
-    });
+});
